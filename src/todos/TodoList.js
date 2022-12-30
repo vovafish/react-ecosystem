1d39d074a296100ca30ca3de6c21ab1994e163f3
Vladimirs <vladimirribakov123@mail.ru>
2022-12-30 21:53:57 +0000

    putting the app together

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "./TodoList.css";
+import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos = [{ text: "Hi" }] }) => (
   <div className="list-wrapper">
+    <NewTodoForm />
     {todos.map((todo) => (
       <TodoListItem todo={todo} />
     ))}
